b4241ef344ac371758ebf0e456023cb6e1d5719c
jahid-opu <ziopu05@gmail.com>
2022-08-04 17:20:04 +0600

    top header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { FiPhoneCall } from "react-icons/fi";
+import { FaRegEnvelope } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>
+    <div className="header_component">
+      <div className="top-header">
+        <Container className="d-flex">
+          <ul className="list-unstyled d-flex align-items-center gap-4">
+            <li>
+              <FiPhoneCall />
+              <a href="tel:09655555555">09655555555</a>
+            </li>
+            <li>
+              <FaRegEnvelope />{" "}
+              <a href="mail-to:support@fvaly.com">support@fvaly.com</a>
+            </li>
+          </ul>
+        </Container>
+      </div>
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
